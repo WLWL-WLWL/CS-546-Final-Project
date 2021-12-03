d817c9c0b13f041fdd584d97350938e476212a13
weili <liuweili@lwlmacbook-air.myfiosgat.com>
2021-12-03 00:48:37 -0500

    add vd

--- data/videogames.js
+++ data/videogames.js
@@ -1,7 +1,24 @@
 const users = mongoCollections.videogames;
 let { ObjectId } = require('mongodb');
 
-
+async function createVideoGame(name, score, releaseDate, developer, genre, price, boxart, comments) {
+    const videogamesCollection = await videogames();
+    let newVideogame = {
+        name: name,
+        score: score,
+        releaseDate: releaseDate,
+        developer: developer,
+        genre: genre,
+        price: price,
+        boxart: boxart,
+        comments: comments
+    }
+    const insertRes = await videogamesCollection.insertOne(newVideogame);
+    if (insertRes.insertedCount === 0) throw 'Could not add restaurant';
+    const newId = insertRes.insertedId;
+    const vg = await this.getVideogameById(newId);
+    return vg;
+}
 
 
 async function getAllVideoGames() {
